6f8c7f2fd63c662479af3a08ecafd27df2c24f11
Louis Choo-Choy <louis.choochoy@duke.edu>
2025-06-01 01:16:28 -0400

    continuing timer

--- frontend/algo-flashcards/src/StudyAlarm.jsx
+++ frontend/algo-flashcards/src/StudyAlarm.jsx
@@ -1,16 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 // import './styles/StudyAlarm.css'
 
 export default function StudyAlarm() {
-    const [timer, setTimer] = useState(0)
-    const id = setInterval(() => {setTimer(prev => prev + 1)})
-    
+  const [timer, setTimer] = useState(0);
+  const [running, setRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+    function handleStart() {
+        if (running) return;
+        intervalRef = setInterval(() => {setTimer((prev) => prev+1)}, 1000)
+    }
+
+    function handleStop() {
+        clearInterval(intervalRef)
+        intervalRef.current = null;
+        setRunning(false);
+    }
+
+    function handleReset() {
+        if (intervalRef.current !== null) {
+            clearInterval(intervalRef.current);
+            intervalRef.current = null;
+        }
+        setRunning(false);
+        setTimer(0);
+    }
+
+    useEffect(() => {
+        return () => {
+            if (intervalRef.current !== null) {
+            clearInterval(intervalRef.current);
+            }
+        };
+    }, []);
+
     return (
         <>
+            <h2>Stopwatch: {timer}</h2>
             <div className="buttons">
-                <button>Start</button>
-                <button>Stop</button>
-                <button>Reset</button>
+                {running ? <button onClick={handleStop}>Stop</button> : <button onClick={handleStart}>Start</button>}
+                <button onClick={handleReset}>Reset</button>
             </div>
         </>
     )
